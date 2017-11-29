1213a71f48f755a08747b6f5fda4b1ce84731098
Haisi Yi <yihaisi@gmail.com>
2017-11-29 00:50:54 -0700

    update data generation

--- Dataset_Generator.py
+++ Dataset_Generator.py
@@ -60,12 +60,12 @@ def get_swiss_roll_dataset_with_labels(numOfSamples):
         label_list.append(label)
     return sample_list, label_list
 
-def get_swiss_roll_dataset_with_labels2(numOfSamples):
-    t = (3 * math.pi / 2) * (1 + 2 * np.random.random(numOfSamples))
-    height = 30 * np.random.random(numOfSamples)
-    X = np.array([t * np.cos(t), height, t * np.sin(t)]) + variance * np.random.normal(0, 1, numOfSamples * 3).reshape(3, numOfSamples)
+def get_swiss_roll_dataset_with_labels2(n):
+    t = (3 * math.pi / 2) * (1 + 2 * np.random.random(n))
+    height = 30 * np.random.random(n)
+    X = np.array([t * np.cos(t), height, t * np.sin(t)]) + variance * np.random.normal(0, 1, n * 3).reshape(3, n)
     labels = np.fmod(np.around(t / 2) + np.around(height / 12), 2)
-    return X.T, labels.reshape(numOfSamples,1)
+    return X.T, labels.reshape(n, 1)
 
 
 def get_broken_swiss_roll_dataset(numOfSamples):
@@ -116,6 +116,15 @@ def get_broken_swiss_roll_dataset_with_label(numOfSamples):
         label_list.append(label)
     return sample_list, label_list
 
+def get_broken_swiss_roll_dataset_with_label2(n):
+    t1 = (3 * math.pi / 2) * (1 + 2 * np.random.random(n // 2) * 0.4)
+    t2 = (3 * math.pi / 2) * (1 + 2 * np.random.random(n // 2) * 0.4 + 0.6)
+    t = np.append(t1, t2)
+    height = 30 * np.random.random(n)
+    X = np.array([t * np.cos(t), height, t * np.sin(t)]) + variance * np.random.normal(0, 1, n * 3).reshape(3, n)
+    labels = np.fmod(np.around(t / 2) + np.around(height / 12), 2)
+    return X.T, labels.reshape(n, 1)
+
 
 def get_helix_dataset(numOfSamples):
     sample_list = []
@@ -154,6 +163,11 @@ def get_helix_dataset_with_label(numOfSamples):
 
     return sample_list, label_list
 
+def get_helix_dataset_with_label2(n):
+    t = np.random.random(n) * 2 * math.pi;
+    X = [(2 + np.cos(8 * t)) * np.cos(t), (2 + np.cos(8 * t)) * np.sin(t), np.sin(8 * t)] + variance * np.random.normal(0, 1, n * 3).reshape(3, n)
+    labels = np.fmod(np.around(t * 1.5), 2);
+    return X.T, labels.reshape(n, 1)
 
 def get_twin_peaks(numOfSamples):
     sample_list = []
@@ -192,6 +206,13 @@ def get_twin_peaks_with_label(numOfSamples):
         label_list.append(label)
     return sample_list, label_list
 
+def get_twin_peaks_with_label2(n):
+    p = 1 - 2 * np.random.random(n)
+    q = 1 - 2 * np.random.random(n)
+    X = [p, q, np.sin(math.pi * p) * np.tanh(3 * q)] + variance * np.random.normal(0, 1, n * 3).reshape(3, n)
+    X[2] *= 10
+    labels = np.fmod(np.sum(np.around((X.T + np.tile(np.amin(X, 1), (n, 1))) * 10), 1), 2)
+    return X.T, labels.reshape(n, 1)
 
 def get_hd_dataset(numOfSamples):
     sample_list = []
@@ -216,3 +237,15 @@ def get_hd_dataset(numOfSamples):
         x_i = x_i.tolist()
         sample_list.append(x_i[0])
     return sample_list
+
+
+def get_hd_dataset_with_label2(n):
+    x1 = np.random.random(n)
+    x2 = np.random.random(n)
+    x3 = np.random.random(n)
+    x4 = np.random.random(n)
+    x5 = np.random.random(n)
+    X = [np.cos(x1), np.tanh(3 * x2), x1 + x3, x4 * np.sin(x2), np.sin(x1 + x5), x5 * np.cos(x2), x5 + x4, x2, x3 * x4, x1]
+    X += variance * np.random.normal(0, 1, n * 10).reshape(10, n)
+    labels = np.fmod(np.around(x1) + np.around(x2) + np.around(x3) + np.around(x4) + np.around(x5) + 1, 2)
+    return X.T, labels.reshape(n, 1)
