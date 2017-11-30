79323d630e7f5ae53648e761c727c74d2dd50ed8
Haisi Yi <yihaisi@gmail.com>
2017-11-29 21:48:23 -0700

    update

--- Plot_Graph.py
+++ Plot_Graph.py
@@ -13,6 +13,7 @@ def plot3D(sample_list):
     ax.set_xlabel('X Label')
     ax.set_xlabel('Y Label')
     ax.set_xlabel('Z Label')
+    plt.show()
 
 def plot3D_color(data, labels):
     x1 = data[labels[:, 0] == 1, 0]
@@ -23,12 +24,12 @@ def plot3D_color(data, labels):
     z0 = data[labels[:, 0] == 0, 2]
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(x1, y1, z1)
-    ax.scatter(x0, y0, z0)
+    ax.scatter(x1, y1, z1, s=5)
+    ax.scatter(x0, y0, z0, s=5)
     ax.set_xlabel('X Label')
     ax.set_xlabel('Y Label')
     ax.set_xlabel('Z Label')
-
+    plt.show()
 
 def plot2D(sample_list):
     sample_x = [x[0] for x in sample_list]
@@ -38,3 +39,4 @@ def plot2D(sample_list):
     ax.scatter(sample_x, sample_y)
     ax.set_xlabel('X Label')
     ax.set_xlabel('Y Label')
+    plt.show()
